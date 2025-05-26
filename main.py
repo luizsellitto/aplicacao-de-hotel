56cb24de767c0e1d0040e3a38dd8f1646be11b62
Lucas <lucaslops12@gmail.com>
2025-05-26 17:06:46 -0300

    estruturando clientes

--- main.py
+++ main.py
@@ -32,6 +32,50 @@ def gravar_arquivo(nome_arquivo, linhas):
 
 # =============== Clientes =========================
 
+def parse_cliente(linha):
+    # Formata em dicionario
+    return
+
+def format_cliente(c):
+    # Converte dicionário para string
+    return f"{c['cpf']};{c['nome']};{c['endereco']};{c['tel_fixo']};{c['tel_cel']};{c['data_nasc'].isoformat()}"
+
+def listar_clientes():
+    return
+
+def buscar_cliente(cpf):
+    return
+
+def incluir_cliente():
+    return
+
+def alterar_cliente():
+
+    return
+
+def excluir_cliente():
+    return
+
+def submenu_clientes():
+    while True:
+        print("\n-- Submenu Clientes --")
+        print("1 Listar todos")
+        print("2 Listar um")
+        print("3 Incluir")
+        print("4 Alterar")
+        print("5 Excluir")
+        print("0 Voltar")
+        op = input("Escolha: ")
+        if op == '1': listar_clientes()
+        elif op == '2':
+            cpf = input("CPF: ").strip()
+            c = buscar_cliente(cpf)
+            print(c or "Não encontrado.")
+        elif op == '3': incluir_cliente()
+        elif op == '4': alterar_cliente()
+        elif op == '5': excluir_cliente()
+        elif op == '0': break
+        else: print("Opção inválida.")
 
 # =============== Reservas =========================
 
